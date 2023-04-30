1ce0be2848f8654a9044c15e0091f88fe0f522b1
Loke-60000 <ramdani.lokman@gmail.com>
2023-04-30 04:10:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MainNavbar from './components/NavBar';
 import Introduction from './pages/Introduction';
 
@@ -19,7 +20,6 @@ function App() {
                 <td>
                   <Routes>
                     <Route path="/" element={<Introduction />} />
-          
                   </Routes>
                 </td>
               </tr>
@@ -27,6 +27,9 @@ function App() {
           </table>
         </div>
       </Router>
+      <div className='copyrights'>
+        <p>2023 Lokman RAMDANI</p>
+      </div>
     </div>
   );
 }
--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CarouselComponent from '../components/Carousel';
+import Amadeus_icon from '../assets/images/amadeus.jpg'
+import Rakugaki_icon from '../assets/images/rakugaki.jpg'
 
 const Introduction = () => {
   const carouselImages = [
@@ -21,21 +23,71 @@ const Introduction = () => {
   ];
 
   return (
-    <div>
-      <CarouselComponent images={carouselImages} />
-      <hr />
-      <div className="bio-container">
-        <h2>Lorem</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          ullamcorper, risus ut facilisis tincidunt, justo sapien bibendum
-          ligula, ac cursus odio ligula in justo. Cras facilisis dui ut sem
-          feugiat, ut egestas orci aliquet. Aenean fringilla neque eget nibh
-          ullamcorper tempus. Proin et sollicitudin mi, id tempor sapien.
-        </p>
+      <div>
+        <CarouselComponent images={carouselImages} />
+        <hr />
+        <div className="bio-container">
+          <h2>About Me</h2>
+          <p>
+            Thank you for visiting my personal website! My name is Lokman RAMDANI, and I'm a 21-year-old web developer who's passionate about the digital world. After obtaining a literary baccalaureate and studying the Japanese language abroad, I made the decision to reorient my career and focus on the field of web development.
+          </p>
+          <h2>Education</h2>
+          <ul>
+            <li>2020: Baccalauréat Générale de Série L - Lycée George Clemenceau</li>
+            <li>2021: Japanese Language Course (6 months) - Tokyo Galaxy, Tokyo, Japan</li>
+            <li>2022-2023: Web Developer and Web Mobile Course - ADRAR</li>
+            </ul>
+            <h2>Work Experience</h2>
+            <ul>
+            <li>2021: Freelance Illustrator - Providing illustration services via the Fiverr platform.</li>
+            <li>Mission Locale - Participated in an intensive program to facilitate education and employment opportunities.</li>
+            </ul>
+            <h2>Interests</h2>
+            <ul>
+            <li>Digital drawing (cartoon, manga)</li>
+            <li>Computer technology (hardware and software)</li>
+            <li>Social media activities: sharing illustrations, streaming</li>
+            <li>Traveling: I've been to Japan and Germany.</li>
+          </ul>
+        </div>
+        <hr />
+        <div className="news-icons-wrapper">
+          <div>
+            <h1>News:</h1>
+            <ul>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+          <div className="Projectsicon">
+            <h1>Projects</h1>
+            <div className="project-container">
+              <a href="https://www.example.com/amadeus">
+                <img className="project_icons" src={Amadeus_icon} alt="Amadeus Fan-Project Realisation 2.0" />
+                <span>Amadeus Fan-Project Realisation 2.0 (name subject to change)</span>
+              </a>
+            </div>
+            <div className="project-container">
+              <a href="https://www.example.com/rakugaki">
+                <img className="project_icons" src={Rakugaki_icon} alt="Rakugaki" />
+                <span>Rakugaki</span>
+              </a>
+            </div>
+          </div>
+
+        </div>
+        
       </div>
-    </div>
-  );
+);
+  
 };
 
 export default Introduction;
