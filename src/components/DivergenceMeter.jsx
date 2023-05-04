e935856627bc28ad4d5c432589fe0b0d8adc4807
Loke-60000 <ramdani.lokman@gmail.com>
2023-05-04 05:24:00 +0200

    divergence meter updated

--- src/components/DivergenceMeter.jsx
+++ src/components/DivergenceMeter.jsx
@@ -1,87 +1,50 @@
-import React, { useRef, useEffect, useState } from 'react';
-import $ from 'jquery';
-
-const NIXIE_RAW = `
-    <div class="nixie">
-        <div class="nixie__wrapper">
-            <div class="nixie__value"></div>
-            <div class="nixie__mesh"></div>
-        </div>
-    </div>
-`;
-
-const INIT_DATA_ATTR = 'is-nixie-inited';
-
-$.fn.nixie = function (options) {
-    if (this.length) {
-        this.each(function () {
-            var opts = $.extend({}, $.fn.nixie.defaultState, options),
-                $this = $(this),
-                isInited = $this.data(INIT_DATA_ATTR),
-                $nixie = $(NIXIE_RAW),
-                thisText = $this.text(),
-                thisClass,
-                thisId;
-
-            if ( ! opts.value && thisText) {
-                opts.value = thisText;
-            }
-
-            if ( ! isInited) {
-                if (thisClass = $this.attr('class')) {
-                    $nixie.addClass(thisClass);
-                }
-
-                if (thisId = $this.attr('id')) {
-                    $nixie.attr('id', thisId);
-                }
-
-                $this.replaceWith($nixie);
-                $nixie.data(INIT_DATA_ATTR, true);
-                $this = $nixie;
-            }
-
-            $.fn.nixie.setState.call($this, opts);
-        });
-    } else {
-        return $.fn.nixie.setState.call($(nixie), $.extend({}, $.fn.nixie.defaultState, options));
-    }
-
-    return this;
-};
-
-$.fn.nixie.setState = function (state) {
-    if (typeof state.value === 'string') {
-        this.find('.nixie__value').text(state.value);
-    }
-
-    return this;
-};
-
-$.fn.nixie.defaultState = {
-    value: ''
-};
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles/nixie.css';
+import tubeImage from '../assets/images/divergence_meter_tube.png';
 
 function Divergencemeter() {
-    const nixieRef = useRef(null);
     const [time, setTime] = useState(new Date());
+    const animationFrameId = useRef();
+
+    const updateClock = () => {
+        setTime(new Date());
+        animationFrameId.current = requestAnimationFrame(updateClock);
+    };
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-
-        return () => clearInterval(intervalId);
+        animationFrameId.current = requestAnimationFrame(updateClock);
+        return () => cancelAnimationFrame(animationFrameId.current);
     }, []);
 
-    useEffect(() => {
-        const formattedTime = `${time.getHours().toString().padStart(2, '0')}.${time.getMinutes().toString().padStart(2, '0')}.${time.getSeconds().toString().padStart(2, '0')}.${Math.floor(time.getMilliseconds() / 10).toString().padStart(2, '0')}`;
-
-        $(nixieRef.current).nixie({ value: formattedTime });
-    }, [time]);
+    const formattedTime = [
+        time.getHours().toString().padStart(2, '0'),
+        time.getMinutes().toString().padStart(2, '0'),
+        time.getSeconds().toString().padStart(2, '0'),
+        time.getMilliseconds().toString().padStart(3, '0')
+    ];
 
     return (
-        <div ref={nixieRef} className='DivergenceMeter'></div>
+        <div className="nixie__wrapper">
+            <div className="nixie__mesh"></div>
+            <div className="nixie__value">
+                {formattedTime.map((segment, index) => (
+                    <React.Fragment key={index}>
+                        {segment.split('').map((digit, digitIndex) => (
+                            <span key={digitIndex} className="nixie__digit-container">
+                                <span className="nixie__digit">{digit}</span>
+                                <span className="nixie__number-background" style={{ backgroundImage: `url(${tubeImage})` }}></span>
+                            </span>
+                        ))}
+                        {index < formattedTime.length - 1 && (
+                            <span className="nixie__digit-container">
+                                <span className="nixie__number-background" style={{ backgroundImage: `url(${tubeImage})` }}></span>
+                                <span className="nixie__dot">.</span>
+                            </span>
+                        )}
+                    </React.Fragment>
+                ))}
+            </div>
+        </div>
     );
 }
 
