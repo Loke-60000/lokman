7e61ff013752ca19faa5e004b2c74cfcd09dc6c7
Loke-60000 <ramdani.lokman@gmail.com>
2023-05-07 05:58:03 +0200

    url fixed for gh-page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,20 +11,20 @@ const MainNavbar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto nav-centered">
               <Nav.Item>
-                <Link className="nav-link" to="/lokman/introduction"><span className="japanese-text">作品紹介</span> <br/> <span className="english-text">Introduction</span></Link>
+                <Link className="nav-link" to="/introduction"><span className="japanese-text">作品紹介</span> <br/> <span className="english-text">Introduction</span></Link>
               </Nav.Item>
 
               <Nav.Item>
-                <Link className="nav-link" to="/lokman/blog"><span className="japanese-text">ニュース</span> <br/> <span className="english-text">News</span></Link>
+                <Link className="nav-link" to="/blog"><span className="japanese-text">ニュース</span> <br/> <span className="english-text">News</span></Link>
               </Nav.Item>
 
               <Nav.Link href="https://github.com/Loke-60000" target="_blank" rel="noopener noreferrer"><span className="japanese-text">ギットハブ</span> <br/> <span className="english-text">Github</span></Nav.Link>
-              <Link className="nav-link" to="/lokman/projects"><span className="japanese-text">プロジェクト</span> <br/> <span className="english-text">Projects</span></Link>
+              <Link className="nav-link" to="/projects"><span className="japanese-text">プロジェクト</span> <br/> <span className="english-text">Projects</span></Link>
               <Nav.Item>
-                <Link className="nav-link" to="/lokman/links"><span className="japanese-text">リンクス</span> <br/> <span className="english-text">Links</span></Link>
+                <Link className="nav-link" to="/links"><span className="japanese-text">リンクス</span> <br/> <span className="english-text">Links</span></Link>
               </Nav.Item>
               <Nav.Item>
-                <Link className="nav-link" to="/lokman/contact"><span className="japanese-text">コンタクト</span> <br/> <span className="english-text">Contact</span></Link>
+                <Link className="nav-link" to="/contact"><span className="japanese-text">コンタクト</span> <br/> <span className="english-text">Contact</span></Link>
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
